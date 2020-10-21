73e8a1251a27c403196d94b2a2e8314161e4ccad
Johannes-Walter <55180891+Johannes-Walter@users.noreply.github.com>
2020-10-21 11:18:08 +0200

    Add files via upload

--- storytime.py
+++ storytime.py
@@ -1,11 +1,9 @@
-from random import choice
-
 print("Weclome to our little story...")
 
 dict = {}
-dict['animal'] = input("What animal is the main character?")
-name = input("What's " + dict['animal'] + "'s name?")
-size = input("How big is" + dict['name'] + "?")
-toy = input("What toy does" + dict[])
-home
-relationship
+dict['animal'] = input("What animal is the main character? ")
+dict['name'] = input("What's " + dict['animal'] + "\'s name? ")
+dict['size'] = input("How big is " + dict['name'] + "? ")
+dict['toy'] = input("What toy does " + dict['name'] + " like the most? ")
+dict['home'] = input("Where does " + dict['name'] + " live? ")
+dict['relationship'] = input("Is " + dict['name'] + " in a relationship? In which kind? ")
