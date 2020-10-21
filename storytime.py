bf241610f6552903ae85a876a792886055173cea
Shara Sivam <65517069+sharasivam@users.noreply.github.com>
2020-10-21 11:08:21 +0200

    Create storytime.py

---
+++ storytime.py
@@ -0,0 +1,11 @@
+from random import choice
+
+print("Weclome to our little story...")
+
+dict = {}
+dict['animal'] = input("What animal is the main character?")
+name = input("What's " + dict['animal'] + "'s name?")
+size = input("How big is" + dict['name'] + "?")
+toy = input("What toy does" + dict[])
+home
+relationship
